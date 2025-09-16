f7753efa74d4164eda18123a6c419eaa9a666d76
Dan Pop <pop.dan05@gmail.comclear>
2025-09-16 17:46:22 +0200

    add yards measurements

--- src/measures/length.js
+++ src/measures/length.js
@@ -1,9 +1,10 @@
 export const length = {
   units: [
-    {names: ['meter', 'meters'], symbols: ['m'], ratio: 1},
-    {names: ['kilometer', 'kilometers'], symbols: ['km'], ratio: 0.001},
-    {names: ['mile', 'miles'], symbols: ['mi'], ratio: 0.000621371},
-    {names: ['foot', 'feet'], symbols: ['ft'], ratio: 3.28084},
-    {names: ['inch', 'inches'], symbols: ['in'], ratio: 39.3701},
+    { names: ['meter', 'meters'], symbols: ['m'], ratio: 1 },
+    { names: ['kilometer', 'kilometers'], symbols: ['km'], ratio: 0.001 },
+    { names: ['mile', 'miles'], symbols: ['mi'], ratio: 0.000621371 },
+    { names: ['foot', 'feet'], symbols: ['ft'], ratio: 3.28084 },
+    { names: ['inch', 'inches'], symbols: ['in'], ratio: 39.3701 },
+    { names: ['yard', 'yards'], symbols: ['yd'], ratio: 1/0.9144 },
   ],
 };
